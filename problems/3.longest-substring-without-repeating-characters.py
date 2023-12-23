70b0b2081ba7ff1c805457fee050c88f104d510b
EmilNichita <doaremil@gmail.com>
2023-12-23 14:47:07 +0100

    solved: problem 3

--- problems/3.longest-substring-without-repeating-characters.py
+++ problems/3.longest-substring-without-repeating-characters.py
@@ -7,6 +7,8 @@
 # @lc code=start
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+
+
         max_size = 0
         n = len(s)
         hashset = set()
@@ -16,7 +18,12 @@ class Solution:
         
         left, right = 0, 1
         hashset.add(s[0])
-        
+
+        # move with two pointers;
+        # at each step, hashset starts by having elements [right...left)
+        # if s[left] not in the hashset, we can increase left. Otherwise,
+        # increase right and remove s[right] from hashset
+        # check if we have a new maximum
         while right < n:
             if s[right] not in hashset:
                 hashset.add(s[right])
