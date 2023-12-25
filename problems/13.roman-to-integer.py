babed8cbc4b0973dc3faba4386fd440d674d1559
EmilNichita <doaremil@gmail.com>
2023-12-25 19:23:52 +0100

    solved: problem 13

---
+++ problems/13.roman-to-integer.py
@@ -0,0 +1,32 @@
+#
+# @lc app=leetcode id=13 lang=python3
+#
+# [13] Roman to Integer
+#
+
+# @lc code=start
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        values = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000,            
+        }
+
+        n = 0
+        max_val_found = -1
+
+        for c in s[::-1]:
+            if values[c] >= max_val_found:
+                n += values[c]
+            else:
+                n -= values[c]
+            max_val_found = max(max_val_found, values[c])
+        return n
+        
+# @lc code=end
+
