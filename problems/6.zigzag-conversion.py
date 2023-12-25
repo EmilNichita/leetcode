d20643bc85e62309d65ada8bc0a4b83c995dc868
EmilNichita <doaremil@gmail.com>
2023-12-25 16:32:34 +0100

    solved: problem 6

---
+++ problems/6.zigzag-conversion.py
@@ -0,0 +1,32 @@
+#
+# @lc app=leetcode id=6 lang=python3
+#
+# [6] Zigzag Conversion
+#
+
+# @lc code=start
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows < 2:
+            return s
+        n = len(s)
+        
+        result = [list() for _ in range(numRows)]
+
+        i = 0
+        while i < n:
+            # going vertically down    
+            for k in range(numRows):
+                if i<n:
+                    result[k].append(s[i])
+                    i+=1
+            for k in range(numRows-2, 0, -1):
+                if i<n:
+                    result[k].append(s[i])
+                    i+=1
+        return "".join(["".join(x) for x in result])
+
+
+            
+# @lc code=end
+
